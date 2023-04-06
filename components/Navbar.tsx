cd698581047d1837b03f7885f4ec800aac7cd890
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-04-05 19:49:35 -0700

    Navigation Update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,40 +4,67 @@
  *  It contains a navigation bar with links to scroll to different sections of the page.
  * 
  */
-import React from 'react';
-import styles from '../styles/components/Navbar.module.css';
+import React, { useState, useEffect, useRef } from "react";
+import styles from "../styles/components/Navbar.module.css";
 
 const Navbar: React.FC = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const headerRef = useRef<HTMLDivElement>(null);
 
-    const scrollToSection = (id: string) => {
-        const sectionElement = document.getElementById(id);
-        if (sectionElement) {
-            sectionElement.scrollIntoView({ behavior: 'smooth' });
-        }
-        };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (headerRef.current && !headerRef.current.contains(event.target as Node)) {
+        setShowDropdown(false);
+      }
+    };
 
-    return (
-        <header className={styles.header}>
-            <nav>
-                <ul className={styles.navList}>
-                    <li className={styles.navItem}>
-                        <span onClick={() => 
-                            scrollToSection('section1')} 
-                            className={styles.navLink}>
-                            Section 1
-                        </span>
-                    </li>
-                    <li className={styles.navItem}>
-                        <span onClick={() => 
-                            scrollToSection('section2')} 
-                            className={styles.navLink}>
-                            Section 2
-                        </span>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const scrollToSection = (id: string) => {
+    const sectionElement = document.getElementById(id);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+      setShowDropdown(false);
+    }
+  };
+
+  return (
+    <header className={styles.header} ref={headerRef}>
+        <div className={styles.hamburgerWrapper}>
+            <span className={styles.brand}>Jeremy Gavrilov</span>
+            <div className={styles.hamburger} onClick={toggleDropdown}>
+                <div className={styles.hamburgerLine}></div>
+                <div className={styles.hamburgerLine}></div>
+                <div className={styles.hamburgerLine}></div>
+            </div>
+        </div>
+        {showDropdown && (
+        <div className={styles.dropdownContainer}>
+            <ul className={styles.dropdownMenu}>
+                <li className={styles.navLink}>
+                    <span onClick={() => scrollToSection("section1")}>
+                        Product 1
+                    </span>
+                </li>
+                <li className={styles.navLink}>
+                    <span onClick={() => scrollToSection("section2")}>
+                        Product 2
+                    </span>
+                </li>
+            </ul>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Navbar;
