2cc5797c4ccad43d59d8b0812a64ff16b7bb7b1a
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-04-12 00:56:50 -0700

    Education Update

--- components/sections/Education.tsx
+++ components/sections/Education.tsx
@@ -1,3 +1,8 @@
+/** Education.tsx
+ * 
+ *  Education component that displays the developer's educational background.
+ * 
+ */
 import React from 'react';
 import style from '../../styles/components/sections/Education.module.css';
 
@@ -7,6 +12,8 @@ type EducationItem = {
   institution: string;
   location: string;
   dates: string;
+  imageUrl: string; 
+  coursework: string[];
 };
 
 const Education: React.FC = () => {
@@ -17,8 +24,12 @@ const Education: React.FC = () => {
       institution: 'Syracuse University',
       location: 'Syracuse, NY',
       dates: '2017 - 2021',
+      imageUrl: 'https://via.placeholder.com/120x120.png',
+      coursework: [ 
+        'Data Structures & Algorithms',
+        'Database Management Systems',
+      ],
     },
-    // Add more education items as needed
   ];
 
   return (
@@ -27,10 +38,27 @@ const Education: React.FC = () => {
       <div className={style.educationContainer}>
         {educationItems.map((item) => (
           <div key={item.id} className={style.educationItem}>
-            <h3 className={style.degree}>{item.degree}</h3>
-            <h4 className={style.institution}>{item.institution}</h4>
-            <p className={style.location}>{item.location}</p>
-            <p className={style.dates}>{item.dates}</p>
+            <div className={style.educationHeader}> 
+              <img
+                src={item.imageUrl}
+                alt={`${item.institution} logo`}
+                className={style.educationImage}
+              />
+              <div>
+                <h3 className={style.degree}>{item.degree}</h3>
+                <h4 className={style.institution}>{item.institution}</h4>
+                <p className={style.location}>{item.location}</p>
+                <p className={style.dates}>{item.dates}</p>
+              </div>
+            </div>
+            <div className={style.coursework}>
+              <h5>Relevant Coursework:</h5>
+              <ul>
+                {item.coursework.map((course, index) => (
+                  <li key={index}>{course}</li>
+                ))}
+              </ul>
+            </div>
           </div>
         ))}
       </div>
