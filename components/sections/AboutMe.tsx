75e030f426bfdc66db6c08b30bbefaae16a6163e
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-04-12 00:18:49 -0700

    About Me Updates

--- components/sections/AboutMe.tsx
+++ components/sections/AboutMe.tsx
@@ -9,24 +9,24 @@ import style from "../../styles/components/sections/AboutMe.module.css";
 const AboutMe: React.FC = () => {
   return (
     <section className={style.aboutMe}>
-      <h2>About Me</h2>
-      <div className={style.aboutMeContent}>
-        <div className={style.aboutMeText}>
-          <p>
-            As a passionate Full Stack Developer holding a Computer Science degree from Syracuse University, 
-            I bring two years of industry experience to the table, along with expertise in APIs, AWS, Java, Web Development, and App Development.
-          </p>
-          <p>
-            I thrive on tackling complex projects and am committed to staying current with cutting-edge technologies and industry best practices.
-            When I'm not immersed in coding, you can find me adventuring through the urban jungle, unwinding with video games or a captivating movie, 
-            or expressing my creativity in the kitchen through baking.
-          </p>
-        </div>
-        <img src="https://via.placeholder.com/200x200.png?text=Jeremy+Gavrilov" alt="Jeremy Gavrilov" className={style.aboutMePhoto} />
-
+      <div className={style.header}>
+        <h2>About Me</h2>
+        <img src="https://via.placeholder.com/250x250.png?text=Jeremy+Gavrilov" alt="Jeremy Gavrilov" className={style.aboutMePhoto} />
+      </div>
+      <div className={style.aboutMeText}>
+        <p>
+          As a passionate Full Stack Developer holding a Computer Science degree from Syracuse University, 
+          I bring two years of industry experience to the table, along with expertise in APIs, AWS, Java, Web Development, and App Development.
+        </p>
+        <p>
+          I thrive on tackling complex projects and am committed to staying current with cutting-edge technologies and industry best practices.
+          When I'm not immersed in coding, you can find me adventuring through the urban jungle, unwinding with video games or a captivating movie, 
+          or expressing my creativity in the kitchen through baking.
+        </p>
       </div>
     </section>
   );
 };
 
 export default AboutMe;
+
