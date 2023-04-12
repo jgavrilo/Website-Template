272fc37d1bf1da89e06d113a8a23602d3af12324
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-04-12 00:43:03 -0700

    Updated Experience
    Update Experience.tsx

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -1,7 +1,13 @@
-
+/** Experience.tsx
+ *
+ * Experience component that displays a list of job experiences
+ * with a circular image next to each job's information.
+ * 
+ */
 import React from 'react';
 import style from '../../styles/components/sections/Experience.module.css';
 
+// Define Job type
 type Job = {
   id: number;
   title: string;
@@ -9,6 +15,7 @@ type Job = {
   location: string;
   dates: string;
   description: string[];
+  imageUrl: string;
 };
 
 const Experience: React.FC = () => {
@@ -19,6 +26,7 @@ const Experience: React.FC = () => {
       company: 'Amazon.com LLC',
       location: 'Seattle, Washington',
       dates: 'July 2021 - March 2023',
+      imageUrl: 'https://via.placeholder.com/100x100.png',
       description: [
         'Design and implement new features for Amazon’s central marketplace and filtering service for personalized recommendations on retail pages.',
         'Handle operations on-call duties to improve service maintainability and provide 99.9% availability during regular rotations and peak events.',
@@ -34,10 +42,19 @@ const Experience: React.FC = () => {
       <div className={style.jobsContainer}>
         {jobs.map((job) => (
           <div key={job.id} className={style.job}>
-            <h3 className={style.jobTitle}>{job.title}</h3>
-            <h4 className={style.jobCompany}>{job.company}</h4>
-            <p className={style.jobLocation}>{job.location}</p>
-            <p className={style.jobDates}>{job.dates}</p>
+            <div className={style.jobHeader}> {/* Add jobHeader div */}
+              <img
+                src={job.imageUrl}
+                alt={`${job.company} logo`}
+                className={style.jobImage} // Add className for job image
+              />
+              <div>
+                <h3 className={style.jobTitle}>{job.title}</h3>
+                <h4 className={style.jobCompany}>{job.company}</h4>
+                <p className={style.jobLocation}>{job.location}</p>
+                <p className={style.jobDates}>{job.dates}</p>
+              </div>
+            </div>
             <ul className={style.jobDescription}>
               {job.description.map((item, index) => (
                 <li key={index}>{item}</li>
